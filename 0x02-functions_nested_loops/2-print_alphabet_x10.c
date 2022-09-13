2495201845264afd40f3a4538d90d6460878e6fc
Merit Dike <dike.merit@yahoo.com>
2022-09-13 15:32:25 -0700

    ch: task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 
 /**
- * Description:  prints 10 times the alphabet, in lowercase.
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase.
  *
  * Return: Always 0
  */
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 {
 	int i;
 
-	for (i = 0; i <=10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		char j;
 
