01b8555bd865c93cd5a353b63a16d5199d6ec600
Merit Dike <dike.merit@yahoo.com>
2022-09-13 15:29:02 -0700

    ch: task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,14 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	
-	for(i = 0; i <=10; i++)
+
+	for (i = 0; i <=10; i++)
 	{
 		char j;
 
-		for(j = 'a'; j <= 'z'; j++)
+		for (j = 'a'; j <= 'z'; j++)
 		{
-			_putchar('a');
+			_putchar(j);
 		}
 			_putchar('\n');
 	}
