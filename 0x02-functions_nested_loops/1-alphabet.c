5e17406ce3a88057b6f66d58ab960329a9195e95
Merit Dike <dike.merit@yahoo.com>
2022-09-13 15:16:09 -0700

    ch: task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Description: prints the alphabet, in lowercase
- *
- * Return: Always 0 (Success)
+ * print_alphabet - prints the alphabet, in lowercase
  */
 
 void print_alphabet(void)
