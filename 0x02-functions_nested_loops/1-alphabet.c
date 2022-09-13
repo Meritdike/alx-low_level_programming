c5c7cf1323e219c878693cda6f70c25c72fae3b7
Merit Dike <dike.merit@yahoo.com>
2022-09-13 14:35:48 -0700

    ch: task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 0; i >= 'z'; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+	return (0);
+}
