da04ce535398be636972e5130352303ad1ed62eb
Merit Dike <dike.merit@yahoo.com>
2022-09-13 16:26:48 -0700

    ch: task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,9 +3,26 @@
 /**
  * print_last_digit - prints the last digit of a number
  *
- * @n: the int to extract the last digit from
+ * @m: the int to extract the last digit from
  *
  * Return: value of the last digit
  */
 
+int print_last_digit(int)
+{
+	int n;
+	if (m < 0)
+	{
+		m = -m;
+		n = m % 10;
 
+		return (n);
+	}
+	else
+	{
+		m = m;
+		n = m % 10;
+
+		return (n);
+	}
+}
