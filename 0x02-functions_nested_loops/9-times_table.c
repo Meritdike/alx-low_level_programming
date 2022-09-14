d62d4c3b08ed3e397712f394a5e4b6b790f7e0f1
Merit Dike <dike.merit@yahoo.com>
2022-09-13 17:10:43 -0700

    ch: task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+	for (a = 0; a < 10; a++)
+	{
+		_putchar('0');
+
+		for (b = 0; b < 10; b++)
+		{
+			c = a * b;
+
+			_putchar(',');
+			_putchar(' ');
+
+			if (c < 10)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((prod / 10) + '0');
+			}
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
