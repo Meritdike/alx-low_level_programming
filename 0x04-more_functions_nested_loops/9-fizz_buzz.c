f6a4f84dd6b48e11858cb816dd8b26fc24c233ab
Merit Dike <dike.merit@yahoo.com>
2022-09-15 15:38:56 -0700

    ch: task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -26,16 +26,13 @@ int main(void)
 		{
 			printf(" Fizz");
 		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
 		else
 		{
-			if (i == 1)
-			{
-				printf("%d", i);
-			}
-			else
-			{
-				printf(" %d", i);
-			}
+			printf(" %d", i);
 		}
 	}
 		printf("\n");
