5cb68763fed44da72cf9c387a3c0b80fae1bc57f
Merit Dike <dike.merit@yahoo.com>
2022-09-19 17:32:47 -0700

    ch: task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int pass[100];
+	int i, j, k;
+
+	j = 0;
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		j += pass[i] + '0';
+		_putchar(pass[i] + '0');
+
+		if ((2772 - sum) - '0' < 78)
+		{
+			k = 2772 - j - '0';
+			j += k;
+			_putchar(k + '0');
+			break;
+		}
+	}
+	return (0);
+}
