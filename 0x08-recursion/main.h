34b9ac2b4bc4c796c1f855a3df3afd26cefe9df5
Merit Dike <dike.merit@yahoo.com>
2022-09-28 13:50:40 -0700

    ch: updated main.h with prototypes used for advanced task

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,8 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int actual_sqrt_recursion(int n, int i);
 int prime(int n, int i);
+int is_palindrome(char *s);
+int _strlen_recursion(char *s);
+int compare_pal(char *s, int i, int len);
+int wildcmp(char *s1, char *s2);
+int substring_match(char *s1, char *s2, char *after_wldcd);
