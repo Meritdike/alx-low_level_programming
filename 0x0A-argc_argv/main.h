7765d437da599416f2e2ab2de2581f5a9dfff66a
Merit Dike <dike.merit@yahoo.com>
2022-09-30 13:11:31 -0700

    ch: updated main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,6 +3,6 @@
 #include <stdbool.h>
 
 int main(int argc __attribute__((unused)), char **argv);
-int main( argc, char **argc __attribute__((unused)));
+int main(int argc, char **argc __attribute__((unused)));
 int main(int argc, char **argv);
 bool num(char *a);
