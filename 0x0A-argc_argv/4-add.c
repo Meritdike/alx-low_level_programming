994edcfab4106f54a42529b74067c2b02cabcc28
Merit Dike <dike.merit@yahoo.com>
2022-09-30 13:55:23 -0700

    ch: task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * num - iterate through each argv to test if it's a number
+ * @a: a argv
+ * Return: true only if entire string is a number, false if not
+ */
+
+bool num(char *a)
+{
+	int i;
+
+	for (i = 0; a[i]; i++)
+	{
+		if (!(a[i] >= 0 && a[i] < 10))
+		{
+			return (0);
+		}
+		return (1);
+	}
+}
+
+/**
+ * main - print sum if all arguments given are numbers
+ * @argc: argument counter
+ * @argv: arguments
+ * Return: 0 on success, 1 if an argument wasn't a number
+ */
+
+int main(int argc, char **argv)
+{
+	int i, sum;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 0; i < argc; i++)
+	{
+		if (num(argv[i]))
+		{
+			sum += atoi (argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+}
