293627ea654c99e8a05d6a32f9085de8b5dd11a0
Merit Dike <dike.merit@yahoo.com>
2022-09-30 14:50:27 -0700

    ch: task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,9 +12,9 @@ int main(int argc, char **argv)
 {
 	int i, minNum = atoi(argv[1]), result = 0;
 
-	int cents[] = {25, 10, 5, 2, 1};
+	int cents[5] = {25, 10, 5, 2, 1};
 
-	if (argc == 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
