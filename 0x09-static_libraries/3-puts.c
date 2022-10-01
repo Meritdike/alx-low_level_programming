4abde88a1d09923c6ac172bcb144fe0ed7e3ccff
Merit Dike <dike.merit@yahoo.com>
2022-09-30 17:36:05 -0700

    ch: task 0

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -5,13 +5,13 @@
  * @str: pointer to the string to print
  */
 
-void _puts(char *str)
+void _puts(char *s)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+		_putchar(s[i]);
 	}
 		_putchar('\n');
 }
