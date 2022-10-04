c5d2bf9dd20b30c722f6e94d59ff5ab8cc457b8f
Merit Dike <dike.merit@yahoo.com>
2022-10-04 16:22:21 -0700

    ch: task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,10 +22,10 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = (int **) malloc(sizeof(int *) * width);
+		arr[i] = (int *) malloc(sizeof(int) * width);
 			if (arr[i] == NULL)
 			{
-				free(arr)
+				free(arr);
 				for (j = 0; j <= i; j++)
 					free(arr[i]);
 				return (NULL);
