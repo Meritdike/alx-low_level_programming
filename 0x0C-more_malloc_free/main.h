3dc54291bdbcc019cc541ac542f31893e2f370da
Merit Dike <65596735+Meritdike@users.noreply.github.com>
2022-10-07 08:49:45 +0100

    ch: updated main.h file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,4 +9,4 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int is_digit(char *s);
 int _strlen(char *s);
 void errors(void);
-int main(int argc, char *argv[]);
+int main(int argc, char **argv);
