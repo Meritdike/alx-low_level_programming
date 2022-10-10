a23c0487cb67297d59b36a125cf7d4d680af6408
Merit Dike <dike.merit@yahoo.com>
2022-10-10 15:53:43 -0700

    ch: task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees dogs.
+ * @d: The dog to be freed.
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d->age);
+		free(d);
+	}
+}
