e68c1855922bfb5641d70483b2a79015bafab917
Merit Dike <dike.merit@yahoo.com>
2022-10-10 15:20:55 -0700

    ch: task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,10 +13,8 @@ void print_dog(struct dog *d)
 		return;
 	else if (d->name == NULL)
 			d->name = "(nil)";
-	else if (d->age == NULL)
-			d->age = "(nil)";
 	else if (d->owner == NULL)
 			d->owner ="(nil)";
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nOwner: %s\n", d->name, d->owner);
 
 }
