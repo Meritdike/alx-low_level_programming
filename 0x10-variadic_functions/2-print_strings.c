cbcd06c34f4275205e9365d5ca86c1154340d27f
Merit Dike <dike.merit@yahoo.com>
2022-10-13 17:04:57 -0700

    ch: task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @separator: separator between strings
+ * @n: number of arguments
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *string;
+	va_list strings;
+
+	va_start(strings, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (string == NULL)
+		{
+			string = "(nil)";
+			printf("%s%s", separator, string);
+		}
+		else if (separator == NULL || (separator && i == 0))
+			printf("%s", string);
+		else
+			printf("%s%s", separator, string);
+	}
+		printf("\n");
+		va_end(strings);
+}
