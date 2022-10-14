56de2b89727549e6ba069819f4040384cc924025
Merit Dike <dike.merit@yahoo.com>
2022-10-13 18:04:55 -0700

    ch: task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,42 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *string, *separator = ", ";
+	va_list listings;
+
+	va_start(listings, format);
+	
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(list, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(list, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(list, double));
+				break;
+			case 's':
+				string = va_arg(listings, char *);
+				if (string == NULL)
+					string = "(nil)";
+				printf("%s%s", separator, string);
+				break;
+			default:
+				i++;
+		}
+			i++;
+	}
+		printf("\n");
+		va_end(listings);
+}
