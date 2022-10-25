cd3df118511b26c2968a77cd092646fd5aca89f5
Merit Dike <dike.merit@yahoo.com>
2022-10-25 15:53:46 -0700

    ch: task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,7 +19,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	}
 
 	if (temp)
-        return (temp);
-    else
-        return (NULL);
+		return (temp);
+	else
+		return (NULL);
 }
