03736dd3746c7b6b5a40c605728ddc16b917fc99
Meritdike <dike.merit@yahoo.com>
2022-10-27 14:21:18 +0000

    ch: task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* clear_bit - set bit to 0 at index
+* @n: number to clear
+* @index: position to clear
+*
+* Return: 1 on success, -1 if index out of range
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int set_bit_value;
+
+	if (index > 50)
+		return (-1);
+
+	set_bit_value = ~(1 << index);
+	*n = *n & set_bit_value;
+
+	return (1);
+}
