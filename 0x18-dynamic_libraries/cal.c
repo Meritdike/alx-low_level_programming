c400df3ad87832a86a1a709c45727e869755de7a
Merit Dike <dike.merit@yahoo.com>
2022-12-12 15:56:46 -0800

    ch: created the cal.c file

---
+++ 0x18-dynamic_libraries/cal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* add - adds two numbers
+* @a: first number to be added
+* @b: second number to be added
+* Return: if success, return result
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* subtract - subtracts two numbers
+* @a: first number to be subtracted
+* @b: second number to be subtracted
+* Return: if success, return result
+*/
+
+int subtract(int a, int b)
+{
+	return (a - b);
+}
