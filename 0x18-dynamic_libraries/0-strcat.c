0843d7baaa9bc94132e3877448b9fde69f2587f8
Meritdike <dike.merit@yahoo.com>
2022-12-12 23:28:03 +0000

    ch: task 0

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ * Return: a pointer to the resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int index, len;
+
+	index = 0;
+	len = 0;
+
+	while (dest[index] != '\0')
+	{
+		index++;
+	}
+
+	while (src[len] != '\0')
+	{
+		dest[index] = src[len];
+		index++;
+		len++;
+	}
+
+	dest[index] = '\0';
+	return (dest);
+}
