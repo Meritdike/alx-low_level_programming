7e9e886502e85253dadcec67db61e5ddbb895356
Meritdike <dike.merit@yahoo.com>
2022-12-13 00:04:19 +0000

    ch: added all calulation functions

--- 0x18-dynamic_libraries/cal.c
+++ 0x18-dynamic_libraries/cal.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* add - adds two numbers
-* @a: first number to be added
-* @b: second number to be added
-* Return: if success, return result
-*/
+ * add - adds two numbers
+ * @a: first number to be added
+ * @b: second number to be added
+ * Return: if success, return result
+ */
 
 int add(int a, int b)
 {
@@ -13,13 +13,49 @@ int add(int a, int b)
 }
 
 /**
-* subtract - subtracts two numbers
-* @a: first number to be subtracted
-* @b: second number to be subtracted
-* Return: if success, return result
-*/
+ * subtract - subtracts two numbers
+ * @a: first number to be subtracted
+ * @b: second number to be subtracted
+ * Return: if success, return result
+ */
 
-int subtract(int a, int b)
+int sub(int a, int b)
 {
 	return (a - b);
+}
+
+/**
+ * mul - multiplys two numbers
+ * @a: first number to be multiplied
+ * @b: second number to be multiplied
+ * Return: if success, return result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first number to be divided
+ * @b: second number to be divided
+ * Return: if success, return result
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - finds the modules between two numbers
+ * @a: first number
+ * @b: second number
+ * Return: if success, return result
+ */
+
+int mod(int a, int b)
+{
+	return (a / b);
 }
